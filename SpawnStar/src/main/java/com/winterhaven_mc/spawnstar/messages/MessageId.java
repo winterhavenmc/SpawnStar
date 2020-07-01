638d18bee5cf4b7548bd41086835132636b51e00
Tim Savage <tim.savage@gmail.com>
2020-07-01 03:12:44 -0500

    update javadocs

--- SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/messages/MessageId.java
+++ SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/messages/MessageId.java
@@ -1,5 +1,8 @@
 package com.winterhaven_mc.spawnstar.messages;
 
+/**
+ * Enum with entries for all player messages in language configuration files
+ */
 public enum MessageId {
 
 	COMMAND_SUCCESS_DESTROY,
