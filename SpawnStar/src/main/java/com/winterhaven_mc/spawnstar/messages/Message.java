54bed66102132061a413db4fd288a7e0d62a1a48
Tim Savage <tim.savage@gmail.com>
2020-07-01 03:16:22 -0500

    update javadocs

--- SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/messages/Message.java
+++ SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/messages/Message.java
@@ -4,14 +4,29 @@ import com.winterhaven_mc.util.AbstractMessage;
 import org.bukkit.command.CommandSender;
 
 
+/**
+ * Class extends AbstractMessage in order to provide a static constructor
+ */
 public class Message extends AbstractMessage<MessageId,Macro> {
 
 
+	/**
+	 * Private class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 */
 	private Message(CommandSender recipient, MessageId messageId) {
 		super(recipient, messageId);
 	}
 
 
+	/**
+	 * Static class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 */
 	public static Message create(CommandSender recipient, MessageId messageId) {
 		return new Message(recipient, messageId);
 	}
