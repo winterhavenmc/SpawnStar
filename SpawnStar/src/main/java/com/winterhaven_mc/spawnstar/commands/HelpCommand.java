ec419b6aed66dd6c18891f177f5d7f8414d0c2b9
savage <tim.savage@gmail.com>
2021-06-15 01:28:04 -0500

    rename languageManager -> languageHandler

--- SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/commands/HelpCommand.java
+++ SpawnStar/src/main/java/com/winterhaven_mc/spawnstar/commands/HelpCommand.java
@@ -64,14 +64,14 @@ public class HelpCommand extends AbstractSubcommand implements Subcommand {
 
 		// if command sender does not have permission to display help, output error message and return true
 		if (!sender.hasPermission(permission)) {
-			Message.create(sender, COMMAND_FAIL_HELP_PERMISSION).send(plugin.languageManager);
+			Message.create(sender, COMMAND_FAIL_HELP_PERMISSION).send(plugin.languageHandler);
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			return true;
 		}
 
 		// check max arguments
 		if (args.size() > getMaxArgs()) {
-			Message.create(sender, COMMAND_FAIL_ARGS_COUNT_OVER).send(plugin.languageManager);
+			Message.create(sender, COMMAND_FAIL_ARGS_COUNT_OVER).send(plugin.languageHandler);
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			displayUsage(sender);
 			return true;
@@ -102,13 +102,13 @@ public class HelpCommand extends AbstractSubcommand implements Subcommand {
 
 		// if subcommand found in map, display help message and usage
 		if (subcommand != null) {
-			Message.create(sender, subcommand.getDescription()).send(plugin.languageManager);
+			Message.create(sender, subcommand.getDescription()).send(plugin.languageHandler);
 			subcommand.displayUsage(sender);
 		}
 
 		// else display invalid command help message and usage for all commands
 		else {
-			Message.create(sender, COMMAND_HELP_INVALID).send(plugin.languageManager);
+			Message.create(sender, COMMAND_HELP_INVALID).send(plugin.languageHandler);
 			plugin.soundConfig.playSound(sender, COMMAND_INVALID);
 			displayUsageAll(sender);
 		}
@@ -121,7 +121,7 @@ public class HelpCommand extends AbstractSubcommand implements Subcommand {
 	 */
 	void displayUsageAll(CommandSender sender) {
 
-		Message.create(sender, COMMAND_HELP_USAGE_HEADER).send(plugin.languageManager);
+		Message.create(sender, COMMAND_HELP_USAGE_HEADER).send(plugin.languageHandler);
 
 		for (String subcommandName : subcommandMap.getKeys()) {
 			if (subcommandMap.getCommand(subcommandName) != null) {
