5f8010280cd5ff474f44653114fc708fa488a507
savage <tim.savage@gmail.com>
2021-06-19 02:50:56 -0500

    get key name from enum member name, lowercased with underscores replaced by dashes

--- SpawnStar/src/test/java/com/winterhaven_mc/spawnstar/ConfigSetting.java
+++ SpawnStar/src/test/java/com/winterhaven_mc/spawnstar/ConfigSetting.java
@@ -1,39 +1,39 @@
 package com.winterhaven_mc.spawnstar;
 
+import java.util.Locale;
+
 public enum ConfigSetting {
 
-    LANGUAGE("language", "en-US"),
-    ENABLED_WORLDS("enabled-worlds", "[]"),
-    DISABLED_WORLDS("disabled-worlds", "[disabled_world1, disabled_world2]"),
-    ITEM_MATERIAL("item-material", "NETHER_STAR"),
-    MINIMUM_DISTANCE("minimum-distance", "10"),
-    TELEPORT_COOLDOWN("teleport-cooldown", "60"),
-    TELEPORT_WARMUP("teleport-warmup", "5"),
-    PARTICLE_EFFECTS("particle-effects", "true"),
-    SOUND_EFFECTS("sound-effects", "true"),
-    SHIFT_CLICK("shift-click", "true"),
-    REMOVE_FROM_INVENTORY("remove-from-inventory", "on-success"),
-    ALLOW_IN_RECIPES("allow-in-recipes", "false"),
-    CANCEL_ON_DAMAGE("cancel-on-damage", "false"),
-    CANCEL_ON_MOVEMENT("cancel-on-movement", "false"),
-    CANCEL_ON_INTERACTION("cancel-on-interaction", "false"),
-    MAX_GIVE_AMOUNT("max-give-amount", "-1"),
-    FROM_NETHER("from-nether", "true"),
-    FROM_END("from-end", "true"),
-    LIGHTNING("lightning", "false"),
-    LOG_USE("log-use", "true");
+    LANGUAGE("en-US"),
+    ENABLED_WORLDS("[]"),
+    DISABLED_WORLDS("[disabled_world1, disabled_world2]"),
+    ITEM_MATERIAL("NETHER_STAR"),
+    MINIMUM_DISTANCE("10"),
+    TELEPORT_COOLDOWN("60"),
+    TELEPORT_WARMUP("5"),
+    PARTICLE_EFFECTS("true"),
+    SOUND_EFFECTS("true"),
+    SHIFT_CLICK("true"),
+    REMOVE_FROM_INVENTORY("on-success"),
+    ALLOW_IN_RECIPES("false"),
+    CANCEL_ON_DAMAGE("false"),
+    CANCEL_ON_MOVEMENT("false"),
+    CANCEL_ON_INTERACTION("false"),
+    MAX_GIVE_AMOUNT("-1"),
+    FROM_NETHER("true"),
+    FROM_END("true"),
+    LIGHTNING("false"),
+    LOG_USE("true");
 
 
-    private final String key;
     private final String value;
 
-    ConfigSetting(String key, String value) {
-        this.key = key;
+    ConfigSetting(String value) {
         this.value = value;
     }
 
     public String getKey() {
-        return this.key;
+        return this.name().toLowerCase().replace('_', '-');
     }
     public String getValue() {
         return this.value;
