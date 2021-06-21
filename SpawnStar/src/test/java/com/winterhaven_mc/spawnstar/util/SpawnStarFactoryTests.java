72470526d7b9dd638f7b4cca756e01e4a1bef892
savage <tim.savage@gmail.com>
2021-06-21 01:46:34 -0500

    reindent with tabs

--- SpawnStar/src/test/java/com/winterhaven_mc/spawnstar/util/SpawnStarFactoryTests.java
+++ SpawnStar/src/test/java/com/winterhaven_mc/spawnstar/util/SpawnStarFactoryTests.java
@@ -13,86 +13,86 @@ import org.junit.jupiter.api.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SpawnStarFactoryTests {
 
-    private ServerMock server;
-//    private WorldMock world;
-    private PluginMain plugin;
+	private ServerMock server;
+	//    private WorldMock world;
+	private PluginMain plugin;
 
-    @BeforeAll
-    public void setUp() {
-        // Start the mock server
-        server = MockBukkit.mock();
+	@BeforeAll
+	public void setUp() {
+		// Start the mock server
+		server = MockBukkit.mock();
 
-        // create mock world
+		// create mock world
 //        world = server.addSimpleWorld("world");
 
-        // start the mock plugin
-        plugin = MockBukkit.load(PluginMain.class);
+		// start the mock plugin
+		plugin = MockBukkit.load(PluginMain.class);
 
-    }
+	}
 
-    @AfterAll
-    public void tearDown() {
-        // Stop the mock server
-        MockBukkit.unmock();
-    }
+	@AfterAll
+	public void tearDown() {
+		// Stop the mock server
+		MockBukkit.unmock();
+	}
 
-    @Nested
-    @DisplayName("Test mocking setup.")
-    class Mocking {
+	@Nested
+	@DisplayName("Test mocking setup.")
+	class Mocking {
 
-        @Test
-        @DisplayName("mock server is not null.")
-        void MockServerNotNull() {
-            Assertions.assertNotNull(server);
-        }
+		@Test
+		@DisplayName("mock server is not null.")
+		void MockServerNotNull() {
+			Assertions.assertNotNull(server);
+		}
 
-        @Test
-        @DisplayName("mock plugin is not null.")
-        void MockPluginNotNull() {
-            Assertions.assertNotNull(plugin);
-        }
-    }
+		@Test
+		@DisplayName("mock plugin is not null.")
+		void MockPluginNotNull() {
+			Assertions.assertNotNull(plugin);
+		}
+	}
 
-    @Nested
-    @DisplayName("Test spawn star factory methods.")
-    class SpawnStarFactory {
+	@Nested
+	@DisplayName("Test spawn star factory methods.")
+	class SpawnStarFactory {
 
-        ItemStack spawnStarItem = plugin.spawnStarFactory.create();
+		ItemStack spawnStarItem = plugin.spawnStarFactory.create();
 
-        @Test
-        @DisplayName("new item type is nether star.")
-        void ItemSetDefaultType() {
-            Assertions.assertEquals(Material.NETHER_STAR, spawnStarItem.getType());
-        }
+		@Test
+		@DisplayName("new item type is nether star.")
+		void ItemSetDefaultType() {
+			Assertions.assertEquals(Material.NETHER_STAR, spawnStarItem.getType());
+		}
 
-        @Test
-        @DisplayName("new item name is SpawnStar.")
-        void NewItemHasDefaultName() {
-            Assertions.assertNotNull(spawnStarItem.getItemMeta());
-            Assertions.assertNotNull(spawnStarItem.getItemMeta().getDisplayName());
-            Assertions.assertEquals("SpawnStar",
-                    ChatColor.stripColor(spawnStarItem.getItemMeta().getDisplayName()));
-        }
+		@Test
+		@DisplayName("new item name is SpawnStar.")
+		void NewItemHasDefaultName() {
+			Assertions.assertNotNull(spawnStarItem.getItemMeta());
+			Assertions.assertNotNull(spawnStarItem.getItemMeta().getDisplayName());
+			Assertions.assertEquals("SpawnStar",
+					ChatColor.stripColor(spawnStarItem.getItemMeta().getDisplayName()));
+		}
 
-        @Test
-        @DisplayName("new item has lore.")
-        void NewItemHasDefaultLore() {
-            Assertions.assertNotNull(spawnStarItem.getItemMeta());
-            Assertions.assertNotNull(spawnStarItem.getItemMeta().getLore());
-            Assertions.assertEquals("Use to Return to World Spawn",
-                    ChatColor.stripColor(String.join(" ",
-                            spawnStarItem.getItemMeta().getLore())));
-        }
+		@Test
+		@DisplayName("new item has lore.")
+		void NewItemHasDefaultLore() {
+			Assertions.assertNotNull(spawnStarItem.getItemMeta());
+			Assertions.assertNotNull(spawnStarItem.getItemMeta().getLore());
+			Assertions.assertEquals("Use to Return to World Spawn",
+					ChatColor.stripColor(String.join(" ",
+							spawnStarItem.getItemMeta().getLore())));
+		}
 
-        @Test
-        void CreateAndTestValidItem() {
-            Assertions.assertTrue(plugin.spawnStarFactory.isItem(spawnStarItem));
-        }
+		@Test
+		void CreateAndTestValidItem() {
+			Assertions.assertTrue(plugin.spawnStarFactory.isItem(spawnStarItem));
+		}
 
-        @Test
-        void ReloadSpawnStarFactory() {
-            plugin.spawnStarFactory.reload();
-            Assertions.assertNotNull(plugin.spawnStarFactory);
-        }
-    }
+		@Test
+		void ReloadSpawnStarFactory() {
+			plugin.spawnStarFactory.reload();
+			Assertions.assertNotNull(plugin.spawnStarFactory);
+		}
+	}
 }
