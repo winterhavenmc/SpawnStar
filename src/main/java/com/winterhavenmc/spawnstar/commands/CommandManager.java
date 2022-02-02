87f15617600d35b8d90f04d3d097f9d445d25549
Tim Savage <tim.savage@gmail.com>
2022-02-01 22:57:32 -0600

    use TabExecutor interface

--- src/main/java/com/winterhavenmc/spawnstar/commands/CommandManager.java
+++ src/main/java/com/winterhavenmc/spawnstar/commands/CommandManager.java
@@ -22,9 +22,8 @@ import com.winterhavenmc.spawnstar.messages.MessageId;
 import com.winterhavenmc.spawnstar.sounds.SoundId;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.command.TabExecutor;
 
 import javax.annotation.Nonnull;
 import java.util.*;
@@ -33,7 +32,7 @@ import java.util.*;
 /**
  * Implements command executor and tab completer for SpawnStar commands.
  */
-public final class CommandManager implements CommandExecutor, TabCompleter {
+public final class CommandManager implements TabExecutor {
 
 	// reference to main class
 	private final PluginMain plugin;
