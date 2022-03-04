6ef30d5e089cc98464266765c26148fbb5776842
Tim Savage <tim.savage@gmail.com>
2022-03-04 05:25:05 -0600

    update BukkitTime and tests

--- src/main/java/com/winterhavenmc/spawnstar/util/BukkitTime.java
+++ src/main/java/com/winterhavenmc/spawnstar/util/BukkitTime.java
@@ -24,62 +24,70 @@ public enum BukkitTime {
 	SECONDS(1000L),
 	MINUTES(60000L),
 	HOURS(3600000L),
-	DAYS(86400000L);
+	DAYS(86400000L),
+	WEEKS(604800000L),
+	MONTHS(2629800000L),
+	YEARS(31557600000L);
 
 	private final long millis;
 
 
- BukkitTime(final long millis) {
+	BukkitTime(final long millis) {
 		this.millis = millis;
 	}
 
-	public long toMillis(final long duration) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / MILLISECONDS.millis;
+	public final long toMillis(final long duration) {
+		return convert(duration, MILLISECONDS);
 	}
 
-	public long toTicks(final long duration) throws IllegalArgumentException {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / TICKS.millis;
+	public final long toTicks(final long duration) {
+		return convert(duration, TICKS);
 	}
 
-	public long toSeconds(final long duration) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / SECONDS.millis;
+	public final long toSeconds(final long duration) {
+		return convert(duration, SECONDS);
 	}
 
-	public long toMinutes(final long duration) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / MINUTES.millis;
+	public final long toMinutes(final long duration) {
+		return convert(duration, MINUTES);
 	}
 
-	public long toHours(final long duration) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / HOURS.millis;
+	public final long toHours(final long duration) {
+		return convert(duration, HOURS);
 	}
 
-	public long toDays(final long duration) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
-		return duration * this.millis / DAYS.millis;
+	public final long toDays(final long duration) {
+		return convert(duration, DAYS);
 	}
 
-	public long convert(long duration, BukkitTime unit) {
-		if (duration < Long.MIN_VALUE / this.millis) throw new IllegalArgumentException("duration would cause underflow.");
-		if (duration > Long.MAX_VALUE / this.millis) throw new IllegalArgumentException("duration would cause overflow.");
+	public final long toWeeks(final long duration) {
+		return convert(duration, WEEKS);
+	}
+
+	public final long toMonths(final long duration) {
+		return convert(duration, MONTHS);
+	}
+
+	public final long toYears(final long duration) {
+		return convert(duration, YEARS);
+	}
+
+	public final long convert(long duration, BukkitTime unit) {
+		if (duration < Long.MIN_VALUE / this.millis) {
+			throw new IllegalArgumentException("duration of " + duration + " " + this + " would cause an underflow in conversion to " + unit + ".");
+		}
+		if (duration > Long.MAX_VALUE / this.millis) {
+			throw new IllegalArgumentException("duration of " + duration + " " + this + " would cause an overflow in conversion to " + unit + ".");
+		}
 		return duration * this.millis / unit.millis;
 	}
 
 	/**
 	 * Get the number of milliseconds for each time unit.
+	 *
 	 * @return the number of milliseconds equal to each time unit
 	 */
-	long getMillis() {
+	public final long getMillis() {
 		return this.millis;
 	}
 
