39095b5703302d8f66f9379d36d43a69d6397704
Tim Savage <tim.savage@gmail.com>
2025-06-30 21:51:43 -0500

    update package name (util -> library)

--- src/main/java/com/winterhavenmc/spawnstar/PluginMain.java
+++ src/main/java/com/winterhavenmc/spawnstar/PluginMain.java
@@ -25,8 +25,8 @@ import com.winterhavenmc.spawnstar.teleport.TeleportHandler;
 import com.winterhavenmc.spawnstar.util.MetricsHandler;
 import com.winterhavenmc.spawnstar.util.SpawnStarUtility;
 import com.winterhavenmc.library.messagebuilder.MessageBuilder;
-import com.winterhavenmc.util.soundconfig.SoundConfiguration;
-import com.winterhavenmc.util.soundconfig.YamlSoundConfiguration;
+import com.winterhavenmc.library.soundconfig.SoundConfiguration;
+import com.winterhavenmc.library.soundconfig.YamlSoundConfiguration;
 import com.winterhavenmc.library.worldmanager.WorldManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
