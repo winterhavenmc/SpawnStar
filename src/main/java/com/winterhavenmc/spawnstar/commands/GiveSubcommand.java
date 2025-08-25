81d8ad9b78c3b7bdc62339f72349c155b15da3cf
Tim Savage <tim.savage@gmail.com>
2025-08-25 08:13:24 -0500

    simplify onTabComplete with switch statement

--- src/main/java/com/winterhavenmc/spawnstar/commands/GiveSubcommand.java
+++ src/main/java/com/winterhavenmc/spawnstar/commands/GiveSubcommand.java
@@ -51,23 +51,12 @@ final class GiveSubcommand extends AbstractSubcommand
 	public List<String> onTabComplete(final CommandSender sender, final Command command,
 	                                  final String alias, final String[] args)
 	{
-		// initialize return list
-		List<String> returnList = new ArrayList<>();
-
-		// return list of matching players
-		if (args.length == 2)
+		return switch (args.length)
 		{
-			return plugin.getServer().matchPlayer(args[1]).stream()
-					.map(Player::getName).collect(Collectors.toList());
-		}
-
-		// return some useful quantities
-		else if (args.length == 3)
-		{
-			returnList = Arrays.asList("1", "2", "3", "5", "10");
-		}
-
-		return returnList;
+			case 2 -> null; // return null for list of matching online players
+			case 3 -> List.of("1", "2", "3", "5", "10"); // return some useful quantities
+			default -> List.of(); // return empty list
+		};
 	}
 
 
